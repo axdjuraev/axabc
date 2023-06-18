3d27d5c4d3e41371075d9ab71a508c9dae8f9d5b
AxDjuraev <axdjuraev@gmail.com>
2023-06-18 14:53:26 +0500

    update: base_repo_collector

--- db/async_uow.py
+++ db/async_uow.py
@@ -17,14 +17,27 @@ class AbstractAsyncUOW(ABC):
 @dataclass
 class BaseRepoCollector(ABC):
     _uow: Union[AbstractAsyncUOW, None] = None
+    _repos = None
 
     def __getattribute__(self, __name: str) -> Any:
-        attr = super().__getattribute__(__name)
+        if self._repos is None:
+            self._repos = {}
 
-        if inspect.isclass(attr) and issubclass(attr, AbstractAsyncRepository):
+            for name_, type_ in self.__class__.__annotations__:
+                if not inspect.isclass(type_):
+                    raise NotImplementedError
+
+                if issubclass(type_, AbstractAsyncRepository):
+                    self._repos[name_] = type_
+
+        cls = self._repos.get(__name)
+
+        if cls:
             if self._uow is None:
                 raise NotImplementedError("uow of the collection is not known")
 
-            return self._uow.get_repo(attr)
+            return self._uow.get_repo(cls)
+
+        attr = super().__getattribute__(__name)
 
         return attr
