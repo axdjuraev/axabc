40394dc3ceb54196670acca3c598d8d19b408bb6
AxDjuraev <axdjuraev@gmail.com>
2023-06-26 08:55:50 +0500

    feat: setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+VERSION = "0.0.1"
+DESCIPTION = "library that defines abstractions for some repeated essences"
+
+with open("requirements.txt", "r") as requirements_file:
+    REQUIREMENTS = requirements_file.readlines()
+
+
+setup(
+    name="axabc",
+    version=VERSION,
+    author="axdjuraev",
+    author_email="<axdjuraev@gmail.com>",
+    description=DESCIPTION,
+    packages=find_packages(),
+    install_requires=REQUIREMENTS,
+)
