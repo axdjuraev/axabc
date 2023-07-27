ef6d40e492e99110c1b343f493d17ff5e360ec68
axdjuraev <axdjuraev@gmail.com>
2023-07-27 15:14:24 +0500

    fix (logging:simple_file_logger): repeating filename

--- axabc/logging/simple_file_logger.py
+++ axabc/logging/simple_file_logger.py
@@ -3,7 +3,7 @@ from .logger_with_id import LoggerWithID
 
 
 class SimpleFileLogger(LoggerWithID):
-    def __init__(self, name, level=logging.DEBUG, filename=__name__):
+    def __init__(self, name, level=logging.DEBUG, filename=...):
         super().__init__(name, level=level)
 
         # Create a formatter to specify the log record's format
@@ -15,7 +15,10 @@ class SimpleFileLogger(LoggerWithID):
         self.addHandler(console_handler)
         
         if filename is not None:
+            filename = name if filename is ... else filename
+
             # Create a file handler to write logs to a file
             file_handler = logging.FileHandler(filename)
             file_handler.setFormatter(formatter)
+            
             self.addHandler(file_handler)
