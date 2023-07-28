91ea345208abbb3927aa42b0dfb523485ab19484
axdjuraev <axdjuraev@gmail.com>
2023-07-28 14:30:13 +0500

    fix (axabc:logging:simple_file_logger): fix not returning result

--- axabc/logging/logger_with_id.py
+++ axabc/logging/logger_with_id.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Any
 from uuid import uuid4, UUID
 from dataclasses import dataclass
 
@@ -11,10 +12,26 @@ class LoggerWithID(logging.Logger):
     def __init__(self, name, level=logging.NOTSET):
         super().__init__(name, level=level)
 
+    def debug(self, msg, *args, **kwargs):
+        return self._log(logging.DEBUG, msg, args, **kwargs)
+
+    def info(self, msg, *args, **kwargs):
+        return self._log(logging.INFO, msg, args, **kwargs)
+
+    def warning(self, msg, *args, **kwargs):
+        return self._log(logging.WARNING, msg, args, **kwargs)
+
+    def error(self, msg, *args, **kwargs):
+        return self._log(logging.ERROR, msg, args, **kwargs)
+
+    def critical(self, msg, *args, **kwargs):
+        return self._log(logging.CRITICAL, msg, args, **kwargs)
+
     def _log(self, level, msg, args, **kwargs):
         unique_id = uuid4()
         msg = f"{msg} [LOG_ID: {unique_id}]"
 
-        super()._log(level, msg, args, **kwargs)
-        
+        if self.isEnabledFor(level):
+            super()._log(level, msg, args, **kwargs)
+
         return LogResult(id=unique_id, msg=msg)
