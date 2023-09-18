87c54678f99512e7f7aba85235d23ff6fce9256d
axdjuraev <axdjuraev@gmail.com>
2023-09-18 09:46:28 +0500

    feat (db): uow

--- axabc/db/uow.py
+++ axabc/db/uow.py
@@ -1,8 +1,9 @@
 from abc import ABC, abstractmethod
-from typing import Generic, Type, Any
+from typing import Generic, Type
 
 from .async_repository import AbstractAsyncRepository
 from .types import TRepo, TRepoCollector
+from .session_mapper import SessionMapper
 
 
 class AbstractUOW(ABC):
@@ -16,36 +17,38 @@ class AbstractUOW(ABC):
 
 
 class UOW(AbstractUOW, Generic[TRepoCollector]):
-    def __init__(self, repo: Type[TRepoCollector], sessions_mapper: dict) -> None:
+    def __init__(self, repo: Type[TRepoCollector], sessions_mapper: SessionMapper) -> None:
         self.repo: TRepoCollector = repo(_uow=self)  # type: ignore
         self.sessions_mapper = sessions_mapper
         self.is_session_closed: bool = False
+        self.used_repos: dict[str, AbstractAsyncRepository] = {}
 
     async def __aenter__(self):
-        await self.session.begin()
         return self
 
     async def __aexit__(self, *args, **kwargs):
         if any(args) or kwargs:
-            await self.session.rollback()
+            await self.dispose()
         else:
-            await self.session.commit()
+            await self.save()
 
         await self.dispose()
 
     async def save(self):
-        await self.session.commit()
+        await self.sessions_mapper.commit()
 
     async def dispose(self):
-        await self.session.close()
+        await self.sessions_mapper.close()
         self.is_session_closed = True
-
-    async def rollback(self):
-        await self.session.rollback()
 
     def get_repo(self, cls: Type[AbstractAsyncRepository]) -> AbstractAsyncRepository:
         if self.is_session_closed:
             raise ValueError("use uow in context manager")
+        
+        repo_name = cls.__name__ 
+        if not repo_name in self.used_repos:
+            session = self.sessions_mapper.require(repo_name)
+            self.used_repos[repo_name] = cls(session)
 
-        return cls(self.sessions_mapper)
+        return self.used_repos[repo_name]
 
