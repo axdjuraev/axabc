65b2a797f97fcef3eb65964b70899e41a58161b3
axdjuraev <axdjuraev@gmail.com>
2023-09-20 11:16:45 +0500

    feat (db:base_schema): jdict

--- axabc/db/base_schemas.py
+++ axabc/db/base_schemas.py
@@ -2,8 +2,17 @@ from datetime import datetime
 from pydantic import BaseModel, Field
 
 
+DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
+
+
 class BaseSchema(BaseModel):
     is_active: bool = True
-    created_at: datetime = Field(default_factory=datetime.now)
-    updated_at: datetime | None = None
+    created_at: datetime = Field(default_factory=datetime.now, strict=False)
+    updated_at: datetime | None = Field(strict=False)
+
+    def jdict(self):
+        res = self.dict()
+        res['created_at'] = self.created_at and self.created_at.strftime(DATETIME_FORMAT)
+        res['updated_at'] = self.updated_at and self.updated_at.strftime(DATETIME_FORMAT)
+        return res
 
