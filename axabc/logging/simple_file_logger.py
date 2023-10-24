f5a389bc1928b0f7a24fd02b2cbecc4fe98c0f6d
axdjuraev <axdjuraev@gmail.com>
2023-10-24 22:27:28 +0500

    ref (SimpleFileLogger): inheritance from SimpleStreamHandler

--- axabc/logging/simple_file_logger.py
+++ axabc/logging/simple_file_logger.py
@@ -1,19 +1,12 @@
 import logging
-from .logger_with_id import LoggerWithID
 from datetime import datetime
 
+from .simple_stream_logger import SimpleStreamLogger
 
-class SimpleFileLogger(LoggerWithID):
+
+class SimpleFileLogger(SimpleStreamLogger):
     def __init__(self, name, level=logging.DEBUG, filename=...):
         super().__init__(name, level=level)
-
-        # Create a formatter to specify the log record's format
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        
-        # Create a console handler to display logs on the console
-        console_handler = logging.StreamHandler()
-        console_handler.setFormatter(formatter)
-        self.addHandler(console_handler)
         
         if filename is not None:
             if filename is ...:
@@ -21,7 +14,8 @@ class SimpleFileLogger(LoggerWithID):
                 filename = f"{name}-{date}.log"
 
             # Create a file handler to write logs to a file
-            file_handler = logging.FileHandler(filename)
-            file_handler.setFormatter(formatter)
+            self.file_handler = logging.FileHandler(filename)
+            self.file_handler.setFormatter(self.formatter)
             
-            self.addHandler(file_handler)
+            self.addHandler(self.file_handler)
+
