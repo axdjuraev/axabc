d56a59283a02f545d657eb69dd162f29af447c2a
axdjuraev <axdjuraev@gmail.com>
2023-10-24 22:29:18 +0500

    ref (logging:init): add SimpleStreamLogger

--- axabc/logging/__init__.py
+++ axabc/logging/__init__.py
@@ -1,5 +1,6 @@
 __all__ = [
     'LoggerWithID',
+    'SimpleStreamLogger',
     'SimpleFileLogger',
     'QualNameLogger',
     'BotLogger',
@@ -7,5 +8,7 @@ __all__ = [
 
 from .bot_logger import BotLogger
 from .logger_with_id import LoggerWithID
+from .simple_stream_logger import SimpleStreamLogger
 from .simple_file_logger import SimpleFileLogger
 from .qualname_logger import QualNameLogger
+
